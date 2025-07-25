6ad28f793d6cb0182aadbc00e6bd69f91aed6285
VastavOne <narayan.bhor01@gmail.com>
2025-07-25 17:58:35 -0500

    Fix asset paths for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/splitchores-landing',
+  assetPrefix: '/splitchores-landing',
   images: {
     unoptimized: true
   }
